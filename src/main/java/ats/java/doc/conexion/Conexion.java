bf083d0f5a0550e4c21b38c0691ad8de85e01c15
JesusAngelMM <jesusangelmartinezmendoza0702@gmail.com>
2024-08-21 15:55:15 -0600

    conexion

--- src/main/java/ats/java/doc/conexion/Conexion.java
+++ src/main/java/ats/java/doc/conexion/Conexion.java
@@ -4,22 +4,27 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 /**
- *
+ * Clase de utilidad para establecer una conexión con una base de datos MySQL.
+ * 
  * @author Jesús Ángel Martínez Mendoza <https://github.com/jangelmm>
  */
 public class Conexion {
-    //Variables de conexión                                       /******
+    // Variables de conexión
     public static final String URL = "jdbc:mysql://localhost:3306/nombre_tabla?useTimeZone=true&serverTimezone=UTC&autoReconnect=true&useSSL=false";
     public static final String usuario = "root";
     public static final String contrasena = "password";
     
-    //Métodos de conexión
-    public Connection getConnection(){
+    /**
+     * Establece y retorna una conexión con la base de datos MySQL.
+     * 
+     * @return un objeto Connection si la conexión es exitosa; null en caso contrario.
+     */
+    public Connection getConnection() {
         Connection conexion = null;
         
-        try{
+        try {
             conexion = DriverManager.getConnection(URL, usuario, contrasena);
-        }catch(Exception ex){
+        } catch (Exception ex) {
             System.out.println("Error, " + ex);
         }
         
